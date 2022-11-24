93c37c43b46b0b22b7b3cea9402fbe739e809eab
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-24 23:19:31 +0600

    Advertised section added

---
+++ src/Pages/Home/AdvertisedItems/AdvertisedItems.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import AdvertisedItem from './AdvertisedItem';
+
+const AdvertisedItems = () => {
+  return (
+    <div>
+      <div class="text-center">
+        <h1 class="text-3xl font-semibold text-gray-800 capitalize lg:text-4xl ">
+          Advertised Phones
+        </h1>
+
+        <p class="max-w-lg mx-auto mt-4 text-gray-500">
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Iure
+          veritatis sint autem nesciunt, laudantium quia tempore delect
+        </p>
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-10 mx-10 my-6">
+        <AdvertisedItem></AdvertisedItem>
+        <AdvertisedItem></AdvertisedItem>
+        <AdvertisedItem></AdvertisedItem>
+      </div>
+    </div>
+  );
+};
+
+export default AdvertisedItems;
