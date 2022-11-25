96ec14b9371da3e50240aa408f4b85bfbe544230
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-25 13:06:03 +0600

    readme file added

--- src/Pages/ErrorPage/ErrorPage.js
+++ src/Pages/ErrorPage/ErrorPage.js
@@ -5,14 +5,18 @@ const ErrorPage = () => {
   return (
     <div>
       {/* <!-- component --> */}
-      <main className="h-screen w-full flex flex-col justify-center items-center bg-[#1A2238]">
-        <h1 className="text-9xl font-extrabold text-white tracking-widest">
-          404
-        </h1>
-        <div className="bg-[#FF6A3D] px-2 text-sm rounded rotate-12 absolute">
-          Page Not Found
+      <main className="h-fit w-full flex flex-col justify-center items-center bg-[#1A2238]">
+        <div>
+          <lottie-player
+            src="https://assets7.lottiefiles.com/packages/lf20_avwze6mc.json"
+            background="transparent"
+            speed="1"
+            loop
+            autoplay
+          ></lottie-player>
         </div>
-        <button className="mt-5">
+
+        <button className="m-5">
           <Link className="relative inline-block text-sm font-medium text-[#FF6A3D] group active:text-orange-500 focus:outline-none focus:ring">
             <span className="absolute inset-0 transition-transform translate-x-0.5 translate-y-0.5 bg-[#FF6A3D] group-hover:translate-y-0 group-hover:translate-x-0"></span>
 
