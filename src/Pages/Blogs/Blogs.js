41c140d2db10c909997031a2503d1b440ef6f47b
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-25 11:29:10 +0600

    bloges section done

--- src/Pages/Blogs/Blogs.js
+++ src/Pages/Blogs/Blogs.js
@@ -1,7 +1,268 @@
 import React from 'react';
 
 const Blogs = () => {
-  return <div></div>;
+  return (
+    <div>
+      <h1 class="text-3xl text-center font-semibold text-white capitalize lg:text-4xl bg-gray-900 pt-6">
+        OUR BLOGS
+      </h1>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="container px-6 py-2 mx-auto">
+          <div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
+            <img
+              class="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96"
+              src="https://bs-uploads.toptal.io/blackfish-uploads/components/blog_post_page/content/cover_image_file/cover_image/1094991/retina_1708x683_image_0-1967657e3078be54d78ccc4d57eae106-f763757d0bd43e58ff9976083b458547-cb44e66cb24bbf150ab28ea5213e176c.png"
+              alt=""
+            />
+
+            <div class="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
+              <p class="text-sm text-blue-500 uppercase">category</p>
+
+              <a
+                href="#"
+                class="block mt-4 text-2xl font-semibold text-gray-800 hover:underline dark:text-white md:text-3xl"
+              >
+                What are the different ways to manage a state in a React
+                application?
+              </a>
+
+              <p class="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
+                There are four main types of state you need to properly manage
+                in your React apps:
+                <li>Local state</li>
+                <li>Global state</li>
+                <li>Server state</li>
+                <li>URL state</li>
+              </p>
+
+              <a
+                href="#"
+                class="inline-block mt-2 text-blue-500 underline hover:text-blue-400"
+              >
+                Read more
+              </a>
+
+              <div class="flex items-center mt-6">
+                <img
+                  class="object-cover object-center w-10 h-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                  alt=""
+                />
+
+                <div class="mx-4">
+                  <h1 class="text-sm text-gray-700 dark:text-gray-200">
+                    Amelia. Anderson
+                  </h1>
+                  <p class="text-sm text-gray-500 dark:text-gray-400">
+                    Lead Developer
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="container px-6 py-2 mx-auto">
+          <div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
+            <img
+              class="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96"
+              src="https://www.educative.io/api/page/6187859468877824/image/download/5404262147293184"
+              alt=""
+            />
+
+            <div class="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
+              <p class="text-sm text-blue-500 uppercase">category</p>
+
+              <a
+                href="#"
+                class="block mt-4 text-2xl font-semibold text-gray-800 hover:underline dark:text-white md:text-3xl"
+              >
+                How does prototypical inheritance work?
+              </a>
+
+              <p class="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
+                The Prototypal Inheritance is a feature in javascript used to
+                add methods and properties in objects. It is a method by which
+                an object can inherit the properties and methods of another
+                object. Traditionally, in order to get and set the [[Prototype]]
+                of an object, we use Object.getPrototypeOf and
+                Object.setPrototypeOf.
+              </p>
+
+              <a
+                href="#"
+                class="inline-block mt-2 text-blue-500 underline hover:text-blue-400"
+              >
+                Read more
+              </a>
+
+              <div class="flex items-center mt-6">
+                <img
+                  class="object-cover object-center w-10 h-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                  alt=""
+                />
+
+                <div class="mx-4">
+                  <h1 class="text-sm text-gray-700 dark:text-gray-200">
+                    Amelia. Anderson
+                  </h1>
+                  <p class="text-sm text-gray-500 dark:text-gray-400">
+                    Lead Developer
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="container px-6 py-2 mx-auto">
+          <div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
+            <img
+              class="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96"
+              src="https://media.geeksforgeeks.org/wp-content/cdn-uploads/20200514113957/What-is-Unit-Testing-and-Why-Developer-Should-Learn-It.png"
+              alt=""
+            />
+
+            <div class="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
+              <p class="text-sm text-blue-500 uppercase">category</p>
+
+              <a
+                href="#"
+                class="block mt-4 text-2xl font-semibold text-gray-800 hover:underline dark:text-white md:text-3xl"
+              >
+                What is a unit test? Why should we write unit tests?
+              </a>
+
+              <p class="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
+                Unit testing is a type of software testing where individual
+                units or software components are tested. Its purpose is to
+                validate that each unit of code performs as expected. A unit can
+                be anything you want it to be — a line of code, a method, or a
+                class.
+                <br />
+                To justify any effort in business, there must be a positive
+                impact on the bottom line. Here are a few benefits to writing
+                unit tests:
+                <li>Unit tests save time and money.</li>
+                <li>
+                  Well-written unit tests act as documentation for your code.
+                </li>
+                <li>
+                  Well-written unit tests act as documentation for your code.
+                </li>
+                <li>It simplifies the debugging process.</li>
+                <li>
+                  Unit testing is an integral part of extreme programming.
+                </li>
+                <li>Unit tests make code reuse easier.</li>
+              </p>
+
+              <a
+                href="#"
+                class="inline-block mt-2 text-blue-500 underline hover:text-blue-400"
+              >
+                Read more
+              </a>
+
+              <div class="flex items-center mt-6">
+                <img
+                  class="object-cover object-center w-10 h-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                  alt=""
+                />
+
+                <div class="mx-4">
+                  <h1 class="text-sm text-gray-700 dark:text-gray-200">
+                    Amelia. Anderson
+                  </h1>
+                  <p class="text-sm text-gray-500 dark:text-gray-400">
+                    Lead Developer
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+      <section class="bg-white dark:bg-gray-900">
+        <div class="container px-6 py-2 mx-auto">
+          <div class="mt-8 lg:-mx-6 lg:flex lg:items-center">
+            <img
+              class="object-cover w-full lg:mx-6 lg:w-1/2 rounded-xl h-72 lg:h-96"
+              src="https://www.simplilearn.com/ice9/free_resources_article_thumb/recact_angular_vue.jpg"
+              alt=""
+            />
+
+            <div class="mt-6 lg:w-1/2 lg:mt-0 lg:mx-6 ">
+              <p class="text-sm text-blue-500 uppercase">category</p>
+
+              <a
+                href="#"
+                class="block mt-4 text-2xl font-semibold text-gray-800 hover:underline dark:text-white md:text-3xl"
+              >
+                React vs. Angular vs. Vue?
+              </a>
+
+              <p class="mt-3 text-sm text-gray-500 dark:text-gray-300 md:text-sm">
+                <b>Angular</b> has a steep learning curve, considering it is a
+                complete solution, and mastering Angular requires you to learn
+                associated concepts like TypeScript and MVC. Even though it
+                takes time to learn Angular, the investment pays dividends in
+                terms of understanding how the front end works.
+                <br />
+                <br />
+                <b>React</b> offers a Getting Started guide that should help one
+                set up React in about an hour. The documentation is thorough and
+                complete, with solutions to common issues already present on
+                Stack Overflow. React is not a complete framework and advanced
+                features require the use of third-party libraries. This makes
+                the learning curve of the core framework not so steep but
+                depends on the path you take with additional functionality.
+                However, learning to use React does not necessarily mean that
+                you are using the best practices.
+                <br />
+                <br />
+                <b>Vue</b> provides higher customizability and hence is easier
+                to learn than Angular or React. Further, Vue has an overlap with
+                Angular and React with respect to their functionality like the
+                use of components. Hence, the transition to Vue from either of
+                the two is an easy option. However, simplicity and flexibility
+                of Vue is a double-edged sword — it allows poor code, making it
+                difficult to debug and test.
+              </p>
+
+              <a
+                href="#"
+                class="inline-block mt-2 text-blue-500 underline hover:text-blue-400"
+              >
+                Read more
+              </a>
+
+              <div class="flex items-center mt-6">
+                <img
+                  class="object-cover object-center w-10 h-10 rounded-full"
+                  src="https://images.unsplash.com/photo-1531590878845-12627191e687?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=764&q=80"
+                  alt=""
+                />
+
+                <div class="mx-4">
+                  <h1 class="text-sm text-gray-700 dark:text-gray-200">
+                    Amelia. Anderson
+                  </h1>
+                  <p class="text-sm text-gray-500 dark:text-gray-400">
+                    Lead Developer
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Blogs;
--- src/Pages/Home/PhoneCategories/PhoneCategory.js
+++ src/Pages/Home/PhoneCategories/PhoneCategory.js
@@ -3,29 +3,24 @@ import React from 'react';
 const PhoneCategory = () => {
   return (
     <div>
-      <div className="w-full overflow-hidden bg-white rounded-lg shadow-lg dark:bg-gray-800 text-center">
+      <div class="flex flex-col items-center justify-center w-full mx-auto">
         <img
-          className="object-cover w-full h-56"
-          src="https://images.unsplash.com/photo-1542156822-6924d1a71ace?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60"
-          alt="avatar"
+          class="w-full h-64 bg-gray-300 bg-center bg-cover rounded-lg shadow-md"
+          src="https://images.unsplash.com/photo-1521903062400-b80f2cb8cb9d?ixlib=rb-1.2.1&ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&auto=format&fit=crop&w=1050&q=80)"
+          alt=""
         />
 
-        <div className="py-5 text-center">
-          <a
-            href="#"
-            className="block text-2xl font-bold text-gray-800 dark:text-white"
-            tabIndex="0"
-            role="link"
-          >
-            John Doe
-          </a>
-          <span className="text-sm text-gray-700 dark:text-gray-200">
-            Software Engineer
-          </span>
+        <div class="w-56 -mt-10 overflow-hidden bg-white rounded-lg shadow-lg md:w-64 dark:bg-gray-800">
+          <h3 class="py-3 font-bold tracking-wide text-center text-gray-800 uppercase dark:text-white">
+            Nike Revolt
+          </h3>
+
+          <div class="flex items-center justify-center px-3 py-2 bg-gray-200 dark:bg-gray-700">
+            <button class="px-3 py-2 text-xs font-semibold text-white uppercase transition-colors duration-300 transform bg-gray-800 rounded hover:bg-gray-700 dark:hover:bg-gray-600 focus:bg-gray-700 dark:focus:bg-gray-600 focus:outline-none">
+              View All
+            </button>
+          </div>
         </div>
-        <button className=" mb-4 px-6 py-2 text-xs font-semibold text-gray-900 uppercase transition-colors duration-300 transform bg-white rounded hover:bg-gray-200 focus:bg-gray-400 focus:outline-none">
-          View All
-        </button>
       </div>
     </div>
   );
