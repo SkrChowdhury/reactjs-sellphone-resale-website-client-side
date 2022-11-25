5d8c0b3438034f237241e63630e5c7362e597b16
Sakkar Chowdhury <38989084+SkrChowdhury@users.noreply.github.com>
2022-11-25 23:34:51 +0600

    firebase added

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyBXea5Qq4Txud9U1u97gx7t-EsN-lWY7Ys',
+  authDomain: 'reactjs-sellphone-resale.firebaseapp.com',
+  projectId: 'reactjs-sellphone-resale',
+  storageBucket: 'reactjs-sellphone-resale.appspot.com',
+  messagingSenderId: '742419822968',
+  appId: '1:742419822968:web:9e8e5a4300a0a9ff96708d',
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
